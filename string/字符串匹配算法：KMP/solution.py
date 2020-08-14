3e64d33a39780bff051273b93c61eb311a7f85f6
cesar.cai <cesar.cai@cesarcaideMacBook-Pro.local>
2020-08-14 23:04:24 +0800

    add string kmp

--- string/字符串匹配算法：KMP/solution.py
+++ string/字符串匹配算法：KMP/solution.py
@@ -1,28 +1,39 @@
 
 class Solution(object):
 
-    def buildNext(self, s, i):
-        cur = s[0:i]
-        print(cur)
-        arr = []
-        number = 0
-        for index in range(1, len(cur)):
-            if cur[0:index] in arr:
-                number = len(cur[0:index])
-            arr.append(cur[0:index])
-            if cur[0:index][::-1] in arr:
-                number = len(cur[0:index])
-            arr.append(cur[0:index][::-1])
-        return number
-        
-
-    def kmp(self, s):
+    def buildNext(self, s):
+        m = len(s)
+        j = 0
         p = []
-        p[0] = -1
-        for i in range(1, len(s)):
-            p[i] = self.buildNext(s, i)
-        print(p)
+        p.append(-1)
+        t = -1
+        while (j < (m - 1)):
+            if (0 > t or s[j] == s[t]):
+                j += 1
+                t += 1
+                p.append(t)
+            else:
+                t = p[t]
+        return p
+    
+    def match(self, find_s, all_s):
+        next = self.buildNext(find_s)
+        m = len(all_s) # 文本串
+        i = 0
+        n = len(find_s) # 模式串
+        j = 0
+
+        while (i < m and j < n):
+            if (0 > j or find_s[j] == all_s[i]):
+                i += 1
+                j += 1
+            else:
+                j = next[j]
+        next = []
+        return i - j
+
+
 
 if __name__ == "__main__":
-    print(Solution().kmp('abcda'))
+    print(Solution().match('ACTGPACY', 'ACTGPACTGKACTGPACY'))
 
