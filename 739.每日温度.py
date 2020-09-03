60f3485154db3a3757e406e385b13c40e9b38eb3
cesarcai <cjh123honey@gmail.com>
2020-09-03 20:56:43 +0800

    add 739

---
+++ 739.每日温度.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=739 lang=python
+#
+# [739] 每日温度
+#
+
+# @lc code=start
+class Solution(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
+
